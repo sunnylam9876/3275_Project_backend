70a5e231acdf3af111e2fff24c747efc1c6f2747
Sunny Lam <77874599+sunnylam9876@users.noreply.github.com>
2024-03-03 15:49:40 -0800

    Initial commit

---
+++ src/test/java/com/example/midterm_mock_3275/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.midterm_mock_3275;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
