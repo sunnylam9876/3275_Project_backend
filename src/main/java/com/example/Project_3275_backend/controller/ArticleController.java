a9455be50ab7b0fa7b13835df7ddec6412853248
Sunny Lam <77874599+sunnylam9876@users.noreply.github.com>
2024-04-05 17:12:51 -0700

    Add-CrossOrigin
    Added @CrossOrigin(origins = "http://localhost:8081") to *Controller.java

--- src/main/java/com/example/Project_3275_backend/controller/ArticleController.java
+++ src/main/java/com/example/Project_3275_backend/controller/ArticleController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +22,10 @@ import com.example.Project_3275_backend.Model.Article;
 import com.example.Project_3275_backend.Model.ArticleRepository;
 import com.example.Project_3275_backend.Model.Guideline;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
+
 public class ArticleController {
 	@Autowired
 	ArticleRepository articleRepository;
