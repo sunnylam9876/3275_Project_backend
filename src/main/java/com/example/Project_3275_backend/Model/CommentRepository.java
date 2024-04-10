aaf73f5d129963f0979dab5ac7a7974d3a50987e
ThomasInDouglas <158655386+ThomasInDouglas@users.noreply.github.com>
2024-04-09 18:56:18 -0700

    Update CommentRepository.java

--- src/main/java/com/example/Project_3275_backend/Model/CommentRepository.java
+++ src/main/java/com/example/Project_3275_backend/Model/CommentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByUserId(long userId);
     List<Comment> findByFlag(int flag);
+    List<Comment> findByArticleId(Long articleId);
 }
